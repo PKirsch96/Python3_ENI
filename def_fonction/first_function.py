9133062e8f1fe17cce11224a7945973f8191a7a1
Paul Kirsch <paul.kirsch96@gmail.com>
2023-03-09 21:03:02 +0100

    paul

--- def_fonction/first_function.py
+++ def_fonction/first_function.py
@@ -7,11 +7,21 @@ def demander_saisie_nombre():
         except:
             pass
         else:
-            if 9 <= saisie <= 99:
+            if 0 <= saisie <= 99:
                 break
     return saisie
 
-print("saisissez le numéro à devinner")
+print("\nl'ordinateur choisi le nombre à devinner\n\n")
 nombre = random.randint(0,100)
 
-print("Essayer de devinner votre propre chiffre")
+print("Essayer de devinner le nombre de l'ordinateur")
+joueur  = demander_saisie_nombre() 
+while nombre != joueur:
+    joueur
+    if nombre > joueur:
+        print("plus grand")
+    else:
+        print("plus petit")
+    joueur  = demander_saisie_nombre() 
+       
+print(f"\nbravo c'était bien {joueur}")
