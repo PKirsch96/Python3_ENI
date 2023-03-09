936b38e35de92e58992a5b2f719742bd21fd039e
Paul Kirsch <paul.kirsch96@gmail.com>
2023-03-09 21:59:41 +0100

    paul

--- def_fonction/first_function.py
+++ def_fonction/first_function.py
@@ -1,5 +1,8 @@
 import random
 
+MIN = 0
+MAX = 99
+
 
 def demander_saisie_nombre(invite, minimum, maximum):
     invite += f"entre {minimum} et {maximum} : "
@@ -14,19 +17,23 @@ def demander_saisie_nombre(invite, minimum, maximum):
                 break
     return saisie
 
+
 print("\nl'ordinateur choisi le nombre à devinner\n\n")
-#nombre = random.randint(0,100)
+# nombre = random.randint(0,100)
 nombre = demander_saisie_nombre(f"Saisissez le nombre à deviner", 0, 50)
 
-print("\n\nEssayer de devinner le nombre de l'ordinateur\n\n") 
+minimum = MIN
+maximum = MAX
+
+print("\n\nEssayer de devinner le nombre de l'ordinateur\n\n")
 while True:
-    joueur  = demander_saisie_nombre(f"Saisissez un nombre", 0, 50) 
+    joueur = demander_saisie_nombre(f"Saisissez un nombre", minimum, maximum)
     if nombre > joueur:
         print("plus grand")
+        minimum = joueur + 1
     elif nombre < joueur:
         print("plus petit")
+        maximum = joueur - 1
     else:
         print(f"\nbravo c'était bien {joueur}")
         break
-       
-
